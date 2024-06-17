d43434ebcdb3911acdf15d5bd0fdec5c65fd08d6
Srivatsa MG <110408410+SrivatsaMG@users.noreply.github.com>
2024-06-17 11:51:40 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'dashboard_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
